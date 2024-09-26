d19fdcfc5a19ffd8f9459d885a3347b294ef1c8d
Thiago2807 <thiagodasilva410@gmail.com>
2024-09-26 20:47:56 -0300

    Gerando classe para gerar a criptografia da senha do usuário.

---
+++ CadastroCliente.API/Application/Utility/HashHelper.cs
@@ -0,0 +1,41 @@
+﻿namespace CadastroCliente.API.Application.Utility;
+
+public class HashHelper
+{
+    private static readonly int Iterations = 10000;
+    private static readonly int HashSize = 32;
+    private static readonly int SaltSize = 32;
+
+    public static byte[] GerarSalt()
+    {
+        byte[] salt = new byte[SaltSize];
+        RandomNumberGenerator.Fill(salt);
+        return salt;
+    }
+
+    public static byte[] GerarHash(string password, byte[] salt)
+    {
+        using var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, salt, Iterations, HashAlgorithmName.SHA256);
+        return rfc2898DeriveBytes.GetBytes(HashSize);
+    }
+
+    private static bool CompararHashes(byte[] hash1, byte[] hash2)
+    {
+        if (hash1.Length != hash2.Length)
+            return false;
+
+        for (int i = 0; i < hash1.Length; i++)
+        {
+            if (hash1[i] != hash2[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    public static bool VerificarSenha(string password, byte[] storedSalt, byte[] storedHash)
+    {
+        byte[] newHash = GerarHash(password, storedSalt);
+        return CompararHashes(newHash, storedHash);
+    }
+}
--- CadastroCliente.API/GlobalUsings.cs
+++ CadastroCliente.API/GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using CadastroCliente.API.IOC;
 global using Microsoft.EntityFrameworkCore;
 global using CadastroCliente.API.IOC.Data;
+
+global using System.Security.Cryptography;
